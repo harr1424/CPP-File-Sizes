542ff3a28689f94c80907eb051498cfcc11e3c1a
harr1424 <harrington59802@protonmail.com>
2022-12-07 19:52:26 -0700

    program working as expected

--- main.cpp
+++ main.cpp
@@ -2,22 +2,44 @@
 #include <vector>
 #include <filesystem>
 
-int main(int argc, char** argv) {
-    if (argc != 2) 
+bool sortByDesc(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b)
+{
+    return a.second > b.second;
+}
+
+void analyze_fs(std::vector<std::pair<std::string, int>> &entry_vec, std::string full_path)
+{
+    for (const auto &entry : std::__fs::filesystem::recursive_directory_iterator(full_path))
+    {
+        if (entry.is_regular_file())
+        {
+            std::pair<std::string, int> new_entry = std::make_pair(entry.path(), entry.file_size());
+            entry_vec.push_back(new_entry);
+        }
+        else if (entry.is_directory() && entry.path() != full_path)
+        {
+            analyze_fs(entry_vec, entry.path());
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    std::vector<std::pair<std::string, int>> entries;
+
+    if (argc != 2)
     {
         std::cout << "Usage: " << argv[0] << " path_to_directory" << std::endl;
-    } 
-    else 
+    }
+    else
     {
         const std::string full_path = argv[1];
+        analyze_fs(entries, full_path);
+        std::sort(entries.begin(), entries.end(), sortByDesc);
 
-        std::vector<std::pair<std::string, int> > entries;
-
-        for (const auto& entry : std::filesystem::recursive_directory_iterator(full_path)) {
-            std::cout << entry.path() << "\t" << entry.file_size() << std::endl;
-            std::pair<std::string, int> new_entry = std::make_pair(entry.path(), entry.file_size());
-            entries.push_back(new_entry);
+        for (int i = 0; i < 10; i++)
+        {
+            std::cout << entries[i].first << "\t" << entries[i].second << std::endl;
         }
-
     }
 }
