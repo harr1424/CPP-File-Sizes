25bbfce5e3000837d3c94585fd57ed6dc4ea9ad8
harr1424 <harrington59802@protonmail.com>
2022-12-09 17:58:58 -0700

    added bounds checking on entries array

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ bool sort_by_desc(const std::pair<std::string, int> &a, const std::pair<std::str
 }
 
 /*
-Recursively visit all filesystem entries at the provided path, and if the entry is a regular file, 
-add its file name and file size to a vector of pairs. If the entry is a directory, this 
+Recursively visit all filesystem entries at the provided path, and if the entry is a regular file,
+add its file name and file size to a vector of pairs. If the entry is a directory, this
 function will be called recursively.
 */
 void analyze_fs(std::vector<std::pair<std::string, int>> &entry_vec, std::string full_path)
@@ -45,9 +45,17 @@ int main(int argc, char **argv)
         std::sort(entries.begin(), entries.end(), sort_by_desc);
 
         // output the ten largest files found
-        for (int i = 0; i < 10; i++)
+        if (entries.size() > 10)
         {
-            std::cout << entries[i].first << "\t" << entries[i].second << std::endl;
+            for (int i = 0; i < 10; i++)
+            {
+                std::cout << entries[i].first << "\t" << entries[i].second << std::endl;
+            }
         }
+        else
+            for (int i = 0; i < entries.size(); i++)
+            {
+                std::cout << entries[i].first << "\t" << entries[i].second << std::endl;
+            }
     }
 }
