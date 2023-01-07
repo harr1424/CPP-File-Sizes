23ff39e3089a865fc2b0aa76beb0cf3447c9e92b
harr1424 <harrington59802@protonmail.com>
2023-01-07 16:13:21 -0700

    completed a fixed size array implementation

--- main.cpp
+++ main.cpp
@@ -4,10 +4,33 @@
 #include <time.h>
 #include <unistd.h>
 
-// function to pass to std::sort in order to sort a vector of pairs by second item in descending order
-bool sort_by_desc(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b)
+struct FS_info
 {
-    return a.second > b.second;
+    std::string name;
+    unsigned long long size;
+};
+
+const size_t NUM_ENTRIES = 10;
+
+// function to pass to std::sort in order to sort a vector of pairs by second item in descending order
+bool sort_by_desc(FS_info const& lhs, FS_info const& rhs)
+{
+    return lhs.size > rhs.size;
+}
+
+// return index of the struct having the smallest file size
+int index_of_smallest_entry(FS_info entries[]) {
+    unsigned long long smallest = entries[0].size;
+    int index = 0;
+
+    for (int i = 0; i < NUM_ENTRIES; i++) {
+        if (entries[i].size < smallest) {
+            smallest = entries[i].size;
+            index = i;
+        }
+    }
+
+    return index;
 }
 
 /*
@@ -15,18 +38,29 @@ Recursively visit all filesystem entries at the provided path, and if the entry 
 add its file name and file size to a vector of pairs. If the entry is a directory, this
 function will be called recursively.
 */
-void analyze_fs(std::vector<std::pair<std::string, int> > &entry_vec, std::string full_path)
+void analyze_fs(FS_info entries[], std::string full_path)
 {
+    static int num_entries = 0;
+
     for (const auto &entry : std::__fs::filesystem::recursive_directory_iterator(full_path))
     {
         if (entry.is_regular_file())
         {
-            std::pair<std::string, int> new_entry = std::make_pair(entry.path(), entry.file_size());
-            entry_vec.push_back(new_entry);
+            if (num_entries < 10)
+            {
+                entries[num_entries] = FS_info{entry.path(), entry.file_size()};
+                num_entries++;
+            }
+            else 
+            {
+                int index = index_of_smallest_entry(entries);
+                entries[index].name = entry.path();
+                entries[index].size = entry.file_size();
+            }
         }
         else if (entry.is_directory())
         {
-            analyze_fs(entry_vec, entry.path());
+            analyze_fs(entries, entry.path());
         }
     }
 }
@@ -57,31 +91,24 @@ int main(int argc, char **argv)
         full_path = argv[1];
     }
 
-    // holds the values of files and their sizes found by analyze_fs()
-    std::vector<std::pair<std::string, int> > entries;
+    FS_info entries[NUM_ENTRIES];
 
     analyze_fs(entries, full_path);
-    std::sort(entries.begin(), entries.end(), sort_by_desc);
+    std::sort(std::begin(entries), std::end(entries), sort_by_desc);
 
     float endTIme = (float)clock() / CLOCKS_PER_SEC;
 
-    // output the ten largest files found
-    if (entries.size() > 10)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            std::cout << entries[i].first << "\t" << entries[i].second << std::endl;
-        }
+    // output the specified number of largest files found
+    for (int i = 0; i < NUM_ENTRIES; i++) {
+        if (entries[i].size > 0) 
+        std::cout << entries[i].name << "\t" << entries[i].size << std::endl;
     }
-    else
-        for (int i = 0; i < entries.size(); i++)
-        {
-            std::cout << entries[i].first << "\t" << entries[i].second << std::endl;
-        }
+    
 
     printf("\nProgram completed in %f seconds", endTIme - startTime);
 
     return EXIT_SUCCESS;
 }
 
-// program completes in about 11 seconds (regardless if the array is sorted or not)
+// program completes in about 10 seconds (regardless if the array is sorted or not)
+// A one second improvement over the unlimited size vector approach 
